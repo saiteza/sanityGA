a100e824ebaa8bc7edd09fe5a496d05d116e5953
Narendra <Narendra.k@techouts.com>
2021-12-15 11:44:29 +0530

    event added

---
+++ pages/gtmmm.js
@@ -0,0 +1,23 @@
+ 
+         
+        
+        
+         function gtmmm() {
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            
+            gtag('config', 'G-S2WDE5Y5ZG',{'debug_mode':true});
+             return (
+                 <div>
+                     hio
+                 </div>
+             )
+         }
+         
+         export default gtmmm
+         
+        
+        
+       
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,15 @@
- 
-import { useState } from "react";
+ import { useState } from "react";
 import { Row, Button } from "react-bootstrap";
 import PageLayout from "components/PageLayout";
 import AuthorIntro from "components/AuthorIntro";
-import CardItem from "components/CardItem";
-import CardListItem from "components/CardListItem";
-import { getAllBlogs } from "libs/api";
+ import { getAllBlogs } from "libs/api";
 import MenuView from "components/Menuview";
-import {getHello} from "actions";
-import {getBlogsData} from "actions";
+ import {getBlogsData} from "actions";
 import { getBlogCards } from "actions/Pagination";
 import PreviewContent from "components/PreviewContent";
- 
- 
+import Script from 'next/script'
 
- export default function Home({ blogs:initialData, preview   }) {
+export default function Home({ blogs:initialData, preview   }) {
  
   const [filter, setfilter] = useState({
     view: {
@@ -35,24 +30,42 @@ import PreviewContent from "components/PreviewContent";
        
       <PageLayout>
         <head>
-        <script id="google-analytics" strategy="afterInteractive">
+
+        <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-S2WDE5Y5ZG"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-S2WDE5Y5ZG',{'debug_mode':true});
+          gtag('config', 'G-S2WDE5Y5ZG');
         `}
-      </script>
-       
-         <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      </Script>
+  <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-TNNKT58');gtag('config', 'G-S2WDE5Y5ZG');`}}></script>
-        <script>
+         
      
+        <script>
+        {`document.querySelector('.card-button').addEventListener('click', function(){
+
+
+
+window.dataLayer.push({
+'event': 'Read More',
+'page': [{
+
+'name': 'Black',
+'brand': 'sample'
+}]
+});});`}
         </script>
+        
           </head>
       {preview&&<PreviewContent />}
 
